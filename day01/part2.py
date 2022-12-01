9b8de6e3298ec2c69b5aa38199a05436955d47ce
Mark Keller <8452750+keller00@users.noreply.github.com>
2022-11-30 21:11:50 -0800

    day01 completed

---
+++ day01/part2.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+
+THIS_DIR = os.path.dirname(__file__)
+
+
+def solve(s: str) -> int:
+
+    lines = s.splitlines()
+    elves = []
+    current_elf = 0
+    for line in lines:
+        if line == "":
+            elves.append(current_elf)
+            current_elf = 0
+        else:
+            current_elf += int(line)
+    elves.append(current_elf)
+
+    # sorted(elves.sort())
+    return sum(sorted(elves, reverse=True)[:3])
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?',
+                        default=os.path.join(THIS_DIR, "input.txt"))
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(solve(f.read()))
+
+    return 0
+
+
+INPUT_S = '''\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'''
+EXPECTED = 45000
+
+
+@pytest.mark.parametrize(
+    ("s", "expected"),
+    (
+        (INPUT_S, EXPECTED),
+        # (helpers.read_file(os.path.join(THIS_DIR, "input.txt")), solution),
+    ),
+)
+def test(s: str, expected: int) -> None:
+    assert solve(s) == expected
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
